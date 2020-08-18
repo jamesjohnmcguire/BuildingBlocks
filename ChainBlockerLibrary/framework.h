6a9897b5b62548e58687c935efcacdfe6cdf62d5
JamesMc <jamesjohnmcguire@gmail.com>
2020-08-18 17:33:20 +0900

    Only include windows.h for Windows based platforms.

--- ChainBlockerLibrary/framework.h
+++ ChainBlockerLibrary/framework.h
@@ -1,5 +1,7 @@
 #pragma once
 
-#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
-// Windows Header Files
-#include <windows.h>
+#if defined _WIN32 || defined __CYGWIN__
+	// Exclude rarely-used stuff from Windows headers
+	#define WIN32_LEAN_AND_MEAN
+	#include <windows.h>
+#endif
